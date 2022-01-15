41bacceda8459fa0477aa4fe2080f0930441457e
Breno Araujo <breno.souza.araujo@hotmail.com>
2022-01-15 18:49:21 -0300

    Pagination Layout 2

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,38 @@
+import MovieCard from "components/MovieCard";
+import MovieScore from "components/MovieScore";
+import MovieStars from "components/MovieStars";
 import Pagination from "components/Pagination";
 import './styles.css';
 
-function Listing(){
+function Listing() {
 
     return (
 
-        <Pagination/>
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <MovieCard />
+                    </div>
+                    <div className="col">
+                        <MovieCard />
+                    </div>
+                    <div className="col">
+                        <MovieCard />
+                    </div>
+                    <div className="col">
+                        <MovieCard />
+                    </div>
+                    <div className="col">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+
+
+        </>
+
     );
 }
 
