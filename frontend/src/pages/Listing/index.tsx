dbb49248148f1d92bb4ae29eac7dbb83e094b139
Breno Araujo <breno.souza.araujo@hotmail.com>
2022-01-16 21:02:58 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,27 +1,33 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
-import MovieScore from "components/MovieScore";
-import MovieStars from "components/MovieStars";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "utils/request";
 import './styles.css';
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
 
-    //forma errada
+const[pageNumber,setPageNumber]= useState(0);
 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(response => {
 
-        console.log(response.data)
+    useEffect(()=>{
 
-    });
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            const data = response.data as MoviePage;
+            setPageNumber(data.number)
+        });
+    }, []);
+
+
 
 
     return (
 
         <>
+        <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
 
